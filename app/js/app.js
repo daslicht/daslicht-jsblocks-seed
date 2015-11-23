8ed7177e0d21959b6ff41483e6ac0a5a520c8e1a
daslicht <daslicht@ansolas.de>
2015-11-23 01:22:43 +0100

    no message

--- app/js/app.js
+++ app/js/app.js
@@ -2,30 +2,64 @@ var App = blocks.Application({
   history: 'pushState'
 });
 
-
-var  Global = blocks.observable(1)
-  
-App.View('Global', {
-  options: {
-    url: 'views/global.html'
+var GlobalModel = App.Model({
+  value: blocks.observable(0),
+  init: function () {
+    var self = this;
+    var timer = setInterval(function () {
+      var currentValue = self.value;
+      self.value(++currentValue);
+    }, 1000);
   }
 });
 
+        App.View('Global', {
+          options: {
+            url: 'views/global.html'
+          },
+          ready: function () {
+            console.log("on");
+          },
+          value: GlobalModel()
+        
+        });
 
-App.View('Home', {
-  options: {
-    route: '/',
-    url: 'views/home.html'
-  }
-  ,global: Global
+
+// var Article = App.Model({
+// 		visible: blocks.observable(),
+// });
+
+var Articles = App.Collection({
+		options: {
+      read: {
+        url: '/articles.json'
+      }
+		}
 });
+
+        App.View('Home', {
+          options: {
+            route: '/',
+            url: 'views/home.html',
+          },
+          //news: Articles([{ foo: 'bar' }])
+          news: Articles(),
+          ready: function () {
+            console.log("on2", Articles().read());
+            
+            
+          },
+        });
+
+
+
 
 App.View('About', {
   options: {
     route: '/about',
     url: 'views/about.html'
   }
-  ,global: Global
+
 });
 
 App.View('Contact', {
@@ -33,5 +67,5 @@ App.View('Contact', {
     route: '/contact',
     url: 'views/contact.html'
   }
-  ,global: Global
+
 });
