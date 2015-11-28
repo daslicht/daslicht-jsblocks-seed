a38da394a3fdf499d44227ae0558f937cb27c68f
daslicht <daslicht@ansolas.de>
2015-11-28 12:16:58 +0100

    now its posting , but empty objects

--- app/js/app.js
+++ app/js/app.js
@@ -21,75 +21,78 @@ var GlobalModel = App.Model({
   init: function () {
     var self = this;
     var timer = setInterval(function () {
-    // You should always call the observable to get it's value in javascript.
-    // It workd here as with the ++ operations the observable function will get called it's 'toString()' function and 
-    // that string will be castet to a number. But this is not a clean way and can lead to hard to find issues.
-    // SO better call it here
-      var currentValue = self.value(); 
+      // You should always call the observable to get it's value in javascript.
+      // It workd here as with the ++ operations the observable function will get called it's 'toString()' function and 
+      // that string will be castet to a number. But this is not a clean way and can lead to hard to find issues.
+      // SO better call it here
+      var currentValue = self.value();
       self.value(++currentValue);
     }, 1000);
   }
 });
 
-        App.View('Global', {
-          options: {
-            url: 'views/global.html'
-          },
-          ready: function () {
-            console.log("on");
-          },
-          value: GlobalModel()
-        
-        });
+App.View('Global', {
+  options: {
+    url: 'views/global.html'
+  },
+  ready: function () {
+    console.log("on");
+  },
+  value: GlobalModel()
+
+});
 
 
 var Article = App.Model({
-	title: blocks.observable(''),
-	author: blocks.observable('')
-});
-// An Collection needs a model to map the stuff on.
-var Articles = App.Collection(Article, {
-	options: {
-		read: {
-        		//url: '/articles.json'
-            url: 'http://ansolas.de:3000/todos'
-    },
+  title: blocks.observable(''),
+  author: blocks.observable(''),
+  options: {
     create: {
       url: 'http://ansolas.de:3000/todos'
     }
-	}
-  
-  
+  }
+
+});
+// An Collection needs a model to map the stuff on.
+var Articles = App.Collection(Article, {
+  options: {
+    read: {
+      //url: '/articles.json'
+      url: 'http://ansolas.de:3000/todos'
+    }
+  }
+
+
 });
 
-        App.View('Home', {
-          options: {
-            route: '/',
-            url: 'views/home.html',
-          },
-          news: Articles().read(), //this works
-          article: Article(),
-          //this.news, //this also works but it has no content to show. 
-            //Starts the ajax read request to the colections 'options.read.url'
-          ready: function () {
-        /*    this.news.add({visible: true, text: 'Some text'});
-         /*   this.news.addMany([{
-            	visible: true,
-            	text: 'other text'
-            },{
-            	visible: true,
-            	text: 'Some much text ...'
-            }]);*/
-           
-          },
-          addItem : function(){
-            console.log('add', this.article.title());
-            this.news.add(this.article);
-            this.article.sync();
-            
-            
-          }
-        });
+App.View('Home', {
+  options: {
+    route: '/',
+    url: 'views/home.html',
+  },
+  news: Articles().read(), //this works
+  article: Article(),
+  //this.news, //this also works but it has no content to show. 
+  //Starts the ajax read request to the colections 'options.read.url'
+  ready: function () {
+    /*    this.news.add({visible: true, text: 'Some text'});
+     /*   this.news.addMany([{
+          visible: true,
+          text: 'other text'
+        },{
+          visible: true,
+          text: 'Some much text ...'
+        }]);*/
+
+  },
+  addItem: function () {
+    console.log('add', this.article.title());
+    this.news.add(this.article);
+    this.article.sync();
+
+
+  }
+});
 
 
 
